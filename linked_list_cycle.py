b4949a920e13882af8baa780dfe731b93a238853
SwarajXO <purarkarswaraj@gmail.com>
2026-02-01 10:06:15 +0530

    linked list cycle

---
+++ linked_list_cycle.py
@@ -0,0 +1,27 @@
+class ListNode:
+    def __init__(self, val =0,next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def hasCycle(self, head):
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+if __name__ == "__main__":
+    node1 = ListNode(1)
+    node2 = ListNode(2)
+    node3 = ListNode(3)
+    node4 = ListNode(4)
+
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node2
+    sol = Solution()
+    print(sol.hasCycle(node1))
