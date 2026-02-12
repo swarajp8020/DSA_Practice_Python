d33fd9bf97cf604846fd0d777ed5bd41418e590c
SwarajXO <purarkarswaraj@gmail.com>
2026-02-12 09:00:09 +0530

    Kth Largest Element Stream

---
+++ kth_largest_element_stream.py
@@ -0,0 +1,23 @@
+import heapq
+
+class KthLargest:
+    def __init__(self, k, nums):
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap)
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+    def add(self, val):
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
+if __name__ == "__main__":
+    k = 3
+    nums = [4,5,8,2]
+    obj = KthLargest(k, nums)
+    print(obj.add(3))
+    print(obj.add(5))
+    print(obj.add(10))
+    print(obj.add(9))
+    print(obj.add(4))
