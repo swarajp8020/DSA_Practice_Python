467dc788297c12574fca8da26603513a4c1f0a3b
SwarajXO <purarkarswaraj@gmail.com>
2026-02-21 10:37:55 +0530

    NumberOfConnectedComponentsDSU

--- number_of_connected_components_DSU.py
+++ number_of_connected_components_DSU.py
@@ -1,6 +1,5 @@
 class Solution:
     def countComponents(self, n, edges):
-
         parent = list(range(n))
         rank = [0] * n
 
@@ -12,10 +11,8 @@ class Solution:
         def union(x, y):
             rootX = find(x)
             rootY = find(y)
-
             if rootX == rootY:
                 return False
-
             if rank[rootX] > rank[rootY]:
                 parent[rootY] = rootX
             elif rank[rootX] < rank[rootY]:
@@ -23,24 +20,16 @@ class Solution:
             else:
                 parent[rootY] = rootX
                 rank[rootX] += 1
-
             return True
-
         components = n
 
         for u, v in edges:
             if union(u, v):
                 components -= 1
-
         return components
-
-
 if __name__ == "__main__":
-
     sol = Solution()
-
     n = 5
     edges = [[0,1],[1,2],[3,4]]
-
     print("Connected Components (Union Find):",
           sol.countComponents(n, edges))
