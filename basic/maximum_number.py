876bd7e3fe23aba42fd27c15c03b7d39725cad2c
SwarajXO <purarkarswaraj@gmail.com>
2026-02-22 09:05:16 +0530

    updated

---
+++ basic/maximum_number.py
@@ -0,0 +1,9 @@
+def find_maximum(nums):
+    max_val = nums[0]
+    for num in nums:
+        if num > max_val:
+            max_val = num
+    return max_val
+if __name__ == "__main__":
+    nums = [12,2,232, 2333]
+    print(find_maximum(nums))
