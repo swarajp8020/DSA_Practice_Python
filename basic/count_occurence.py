069f3f3811877430a1e3a42eaf7f09cb02a23cfb
SwarajXO <purarkarswaraj@gmail.com>
2026-02-23 09:08:04 +0530

    count_occurence

--- basic/count_occurence.py
+++ basic/count_occurence.py
@@ -4,6 +4,7 @@ def count_occurence(nums, target):
         if num == target:
             count += 1
     return count
+
 if __name__ == "__main__":
     nums = [1,2,3,2,2,5]
     print(count_occurence(nums, 2))
