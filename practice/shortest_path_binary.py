7e3fe5e77838dcbc02322f09bfcc56389aa9bbe8
SwarajXO <purarkarswaraj@gmail.com>
2026-03-01 10:14:53 +0530

    def shortestPathMatrix(grid):

--- practice/shortest_path_binary.py
+++ practice/shortest_path_binary.py
@@ -1,28 +1,37 @@
-from collections import deque  
+from collections import deque
 
-class Solution:
-    def shortestPathBinaryMatrix(self, grid):
-        n = len(grid)  
-        
-        if not grid or grid[0][0] != 0 or grid[n-1][n-1] != 0:  
-            return -1  
-        directions = [(1,0),(-1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]  
-        
-        queue = deque([(0,0,1)])  
-        grid[0][0] = 1  
-        
-        while queue:  
-            row, col, dist = queue.popleft()  
-            
-            if row == n-1 and col == n-1: 
-                return dist  
-            
-            for dr, dc in directions: 
-                nr, nc = row + dr, col + dc  
-                
-                if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0:
-                    
-                    grid[nr][nc] = 1  
-                    queue.append((nr, nc, dist + 1))  
-        
-        return -1  
+def shortestPathMatrix(grid):
+    n = len(grid)
+
+    if n == 0:
+        return -1
+    
+    if grid[0][0] != 0 or grid[n-1][n-1] != 0:
+        return -1
+    
+    directions = [
+        (-1,-1), (-1, 0), (-1,-1),
+        (0,-1),           (0,1),
+        (1,-1), (1, 0), (1,1)
+    ]
+    queue = deque()
+    queue.append((0,0,1))
+    grid[0][0] = 1
+    while queue:
+        row, col, distance = queue.popleft()
+
+        if row == n - 1 and col == n - 1:
+            return distance
+        for dr, dc in directions:
+            new_row = row + dr
+            new_col = col + dc
+
+            if 0 <= new_row and 0 <= new_col < n:
+                if grid[new_row][new_col] == 0:
+                    grid[new_row][new_col] = 1
+                    queue.append((new_row, new_col, distance + 1))
+    return -1
+
+if __name__ == "__main__":
+    grid = [[0,0,0],[1,1,0],[1,1,0]]
+    print(shortestPathMatrix(grid))
