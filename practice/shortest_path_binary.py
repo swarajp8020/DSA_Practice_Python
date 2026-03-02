bb85eef6e54e60acd5633d8aca661ba9379be097
SwarajXO <purarkarswaraj@gmail.com>
2026-03-02 08:33:09 +0530

    shortestPathBinaryMatrix

--- practice/shortest_path_binary.py
+++ practice/shortest_path_binary.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-def shortestPathMatrix(grid):
+def shortestPathBinaryMatrix(grid):
     n = len(grid)
 
     if n == 0:
@@ -10,27 +10,27 @@ def shortestPathMatrix(grid):
         return -1
     
     directions = [
-        (-1, -1), (-1,0), (-1,1),
-        (0, -1),           (0,1),
-        (1, -1), (1, 0), (1, 1)
+        (-1,-1),(-1,0),(-1,1),
+        (0,-1),       (0,1),
+        (1,-1), (1,0), (1,1)
     ]
     queue = deque()
     queue.append((0,0,1))
     grid[0][0] = 1
     while queue:
-        row, col, distance = queue.popleft()
-        if row == n - 1 and col == n-1:
+        row,col,distance = queue.popleft()
+        if row == n-1 and col == n-1:
             return distance
-        for dr, dc in directions:
+        for dr,dc in directions:
             new_row = row + dr
             new_col = col + dc
-            
+
             if 0 <= new_row < n and 0 <= new_col < n:
                 if grid[new_row][new_col] == 0:
                     grid[new_row][new_col] = 1
-                    queue.append((new_row, new_col, distance + 1))
+                    queue.append((new_row,new_col, distance + 1))
     return -1
 
 if __name__ == "__main__":
-    grid = [[0,0,0],[1,1,0],[1,1,0]]
-    print(shortestPathMatrix(grid))
+    grid = [[1,0,0],[1,1,0],[1,1,0]]
+    print(shortestPathBinaryMatrix(grid))
