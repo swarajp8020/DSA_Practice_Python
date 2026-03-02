0f6ecc78e9f59448a56a656beb4371d5169206f1
SwarajXO <purarkarswaraj@gmail.com>
2026-03-02 09:21:50 +0530

    min_time_to_resolve_bugs

---
+++ practice/min_time_to_resolve_bugs.py
@@ -0,0 +1,24 @@
+def min_time_to_resolve_bugs(timeTaken, bugs):
+    min_time = min(timeTaken)
+    low = 1
+    high = bugs * min_time
+    answer = high
+    while low <= high:
+        mid = low + (high - low)// 2
+        total_bugs_resolved = 0
+        for time_per_bug in timeTaken:
+            total_bugs_resolved += mid // time_per_bug
+
+            if total_bugs_resolved >= bugs:
+                break
+        if total_bugs_resolved >= bugs:
+            answer = mid
+            high = mid -1
+        else:
+            low = mid + 1
+    return answer
+
+if __name__ == "__main__":
+    timeTaken = [1,2,3]
+    bugs = 5
+    print(min_time_to_resolve_bugs(timeTaken, bugs))
