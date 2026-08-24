9afb837c7aec76869e4187cc84ae0485fec341b1
SwarajXO <purarkarswaraj@gmail.com>
2026-08-24 07:34:04 +0530

    24 august

---
+++ august_2026/24_august.py
@@ -0,0 +1,8 @@
+customer_id = "CUST1001"
+
+customer_hash = hash(customer_id)
+print("Customer ID:", customer_id)
+print(hash(customer_id))
+print("Hash:", customer_hash)
+print(hash("CUST1001"))
+print(hash("CUST1002"))
